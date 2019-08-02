5dda79d57489a5b2e07d86ffa13dfb01cf6478a3
Hunter Maxfield <you@example.com>
2019-08-01 22:19:25 -0600

    added comp stat section

--- app.py
+++ app.py
@@ -52,162 +52,164 @@ def about():
 
 @socketio.on('connection_msg')
 def connected(message):
-    
+
     data = message
     print(data)
 
 
 @socketio.on('forecast_settings')
 def forecast_settings(message):
-    
+
     # Initial forecast settings - the first time the user sends forecast settings through the app - will use this value in forecastr method
     build_settings = 'initial'
-    
+
     # store message['data'] into a df called data
     data = message['data']
-    
+
     # Keep Original Data in Exisiting Structure
     original_dataset = data[1]['data'][1]['data']
-    
+
     #print("******************** ORIGINAL DATASET *****************************")
     #print(original_dataset)
     #print("******************** ORIGINAL DATASET *****************************")
-    
+
     # Extract info from forecast_settings message
     time_series_data = pd.DataFrame(data[1]['data'][1]['data'])
     forecast_settings = data[0]
     freq = data[2]
     column_headers = data[1]['data'][0]
-    
+
     # Format the date and metric unit
     time_unit = column_headers[0]
+    print(time_unit)
     time_series_data[time_unit] = time_series_data[time_unit].apply(lambda x: pd.to_datetime(str(x)))
     metric = column_headers[1]
-    
-    # y (aka as "the original data for the metric being forecasted") will be used in the chartjs line graph 
+
+    # y (aka as "the original data for the metric being forecasted") will be used in the chartjs line graph
     y = time_series_data[metric].tolist()
-    
+
     # Use Facebook Prophet through forecastr method
     forecast = forecastr(time_series_data,forecast_settings,column_headers,freq,build_settings)
-        
+
     # Need to convert forecast back into a list / array for y, y_hat and date so it can be properly graphed with chartjs
     y_hat = forecast[0]
     dates = forecast[1]
     model = forecast[2]
     csv_export = forecast[3]
     forecasted_vals = forecast[4]
+    forecasted_vals_mean = forecast[5]
 
 
     # Send data back to the client
-    data_back_to_client = [dates,y_hat,y,forecast_settings,column_headers,freq,original_dataset,csv_export, forecasted_vals]
+    data_back_to_client = [dates,y_hat,y,forecast_settings,column_headers,freq,original_dataset,csv_export, forecasted_vals, forecasted_vals_mean]
     #print(data_back_to_client)
-    
-    
+
+
     emit('render_forecast_chart', {'data': data_back_to_client})
-    
-    
-    
+
+
+
     # Validate Model
     #mape_score = validate_model(model,dates)
 
     #emit('model_validation', {'data':mape_score})
-    
-    
+
+
 @socketio.on('update_chart_settings')
 def update_chart(message):
-    
+
     # This is an update to the initial forecast settings. The user has changed their settings on Step 3, so we set build_settings to update.
     build_settings = 'update'
-    
+
     data = message['data']
-    
-    ### Setup variables for use in the forecastr method    
+
+    ### Setup variables for use in the forecastr method
     time_series_data = data[4]
     original_dataset = time_series_data
     time_series_data = pd.DataFrame(time_series_data)
-    
+
     #print("********* TIME SERIES DF ****************")
     #print(time_series_data.head())
     #print("********* TIME SERIES DF ****************")
-    
+
     forecast_settings = data[1]
     column_headers = data[2]
     freq = data[3]
-    
+
     # Dimension and Metric
     time_unit = column_headers[0]
     metric = column_headers[1]
-    
+
     # Make sure time_unit is converted to datetime in order to join in helper_v3
     time_series_data[time_unit] = time_series_data[time_unit].apply(lambda x: pd.to_datetime(str(x)))
 
-    
+
     #print([time_unit,metric])
-    
+
     # Original Data
     y = time_series_data[metric].tolist()
-    
+
     # Use Facebook Prophet through forecastr method
     forecast = forecastr(time_series_data,forecast_settings,column_headers,freq,build_settings)
-    
+
     # Need to convert forecast back into a list / array for y, y_hat and date so it can be properly graphed with chartjs
     y_hat = forecast[0]
     dates = forecast[1]
     model = forecast[2]
     csv_export = forecast[3]
     forecasted_vals = forecast[4]
-    
+
     # Send data back to the client - took out original dataset
     data_back_to_client = [dates,y_hat,y,forecast_settings,column_headers,freq,original_dataset,csv_export,forecasted_vals]
     emit('render_forecast_chart', {'data': data_back_to_client})
-    
+
     # Validate Model
     #mape_score = validate_model(model,dates)
-    
+
     #emit('model_validation', {'data':mape_score})
-    
-    
-    
-@socketio.on('reset')    
+
+
+
+@socketio.on('reset')
 def reset(message):
-    
+
     data = message['data']
     #print(data)
 
-    
+
 @socketio.on('send_csv')
 def main(message):
-        
+
     # Store message['data'] in data
     data = message['data']
-    
+
     # Convert data to a pandas DataFrame
     data = pd.DataFrame(data)
-    
+
     #print(data)
 
     # Let's do some preprocessing on this data to determine which column is the dimension vs. metric.
     column_headers = preprocessing(data)
-    
+
     # Set the time unit and metrc unit names
     time_unit = column_headers[0]
     metric_unit = column_headers[1]
-    
+
     # Determine whether the timeframe is daily, weekly, monthly, or yearly
     timeframe = determine_timeframe(data, time_unit)
-    
+
     # Get summary statistics about original dataset
     summary_stats = get_summary_stats(data,column_headers)
-    
+
     # Send original data to a list
     dimension = data[time_unit].tolist()
     metric = data[metric_unit].tolist()
-    
+
     original_data = [dimension,metric]
-    
+
     # Send data back to the client in the form of a label detected or text extracted.
     emit('render_uploaded_csv_data', {'data': [column_headers,message, timeframe, summary_stats,original_data]})
-    
+
 
 if __name__ == '__main__':
     socketio.run(app, log_output=False)
